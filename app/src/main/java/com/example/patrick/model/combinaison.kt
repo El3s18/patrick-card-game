f5805e94fc23dd530e70e3dfa7a5d94d013c04a6
eles1808 <alexandre.sabatepro@gmail.com>
2026-07-16 23:36:25 +0200

    Ajout des mécanique de combinaisons complexes

--- app/src/main/java/com/example/patrick/model/combinaison.kt
+++ app/src/main/java/com/example/patrick/model/combinaison.kt
@@ -15,3 +15,32 @@ fun estUnCarre(cartes: List<Carte>): Boolean {
     return cartes[0].valeur == cartes[1].valeur && cartes[1].valeur == cartes[2].valeur && cartes[2].valeur == cartes[3].valeur
 }
 
+fun estUnZobi(cartes: List<Carte>): Boolean {
+    if (cartes.size != 3) return false
+    val cartesTriees = cartes.sortedBy { it.valeur.ordinal }
+    return cartesTriees[1].valeur.ordinal == cartesTriees[0].valeur.ordinal + 1 &&
+            cartesTriees[2].valeur.ordinal == cartesTriees[1].valeur.ordinal + 1
+}
+
+fun estUnZobiLaMouche(cartes: List<Carte>): Boolean {
+    if (cartes.size != 5) return false
+    val cartesTriees = cartes.sortedBy { it.valeur.ordinal }
+
+    for (i in 0..3) {
+        if (cartesTriees[i + 1].valeur.ordinal != cartesTriees[i].valeur.ordinal + 1) {
+            return false
+        }
+    }
+    return true
+}
+
+fun estUneCouleur(cartes: List<Carte>): Boolean {
+    if (cartes.size != 5) return false
+
+    for (i in 0..3) {
+        if (cartes[i + 1].famille != cartes[i].famille) {
+            return false
+        }
+    }
+    return true
+}
