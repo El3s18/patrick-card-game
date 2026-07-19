ed2c2993b9acc724d6ad5324c1c8383ccf1b6eb3
eles1808 <alexandre.sabatepro@gmail.com>
2026-07-19 15:27:46 +0200

    Ajout des dependances Firebase Auth et Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 }
